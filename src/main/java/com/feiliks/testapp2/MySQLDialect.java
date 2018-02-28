5e2082d4fc113ea5d1047a03988c7a74ea0ac60a
Zhiji Gu <zhiji_gu@feiliks.com>
2018-02-28 09:42:25 +0800

    fix mysql case-sensitivity issue

---
+++ src/main/java/com/feiliks/testapp2/MySQLDialect.java
@@ -0,0 +1,12 @@
+package com.feiliks.testapp2;
+
+import org.hibernate.dialect.MySQL5InnoDBDialect;
+
+public class MySQLDialect extends MySQL5InnoDBDialect {
+
+    @Override
+    public String getTableTypeString() {
+        return " CHARACTER SET utf8 COLLATE utf8_bin ENGINE InnoDB";
+    }
+
+}
