5ffe7844a6b53ef874b760d9009e925261de4a5f
Zhiji Gu <zhiji_gu@feiliks.com>
2018-03-01 11:10:28 +0800

    password hashing on logging-in

--- src/main/java/com/feiliks/testapp2/controllers/AccountController.java
+++ src/main/java/com/feiliks/testapp2/controllers/AccountController.java
@@ -1,27 +1,20 @@
 package com.feiliks.testapp2.controllers;
 
-import com.feiliks.testapp2.AuthTokenUtil;
-import com.feiliks.testapp2.KeyPairProvider;
-import com.feiliks.testapp2.TokenAuthFilter;
-import com.feiliks.testapp2.ValidationException;
+import com.feiliks.testapp2.*;
 import com.feiliks.testapp2.dto.LoginDTO;
 import com.feiliks.testapp2.dto.Message;
-import com.feiliks.testapp2.dto.UserDTO;
+import com.feiliks.testapp2.dto.UserWithPermissionsDTO;
 import com.feiliks.testapp2.jpa.entities.User;
 import com.feiliks.testapp2.jpa.repositories.UserRepository;
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/account")
@@ -41,7 +34,8 @@ public class AccountController extends AbstractController {
             throw new ValidationException(result);
         }
         User u = userRepository.findByUsername(login.getUsername());
-        if (u != null && u.getPassword().equals(DigestUtils.sha256Hex(login.getPassword()))) {
+        if (u != null && u.getPassword().equals(
+                PasswordUtil.hash(login.getUsername(), login.getPassword()))) {
             String token = AuthTokenUtil.create(
                     u, keyPairProvider.getSigner()).getEncoded();
             Message msg = new Message("success", token);
@@ -60,11 +54,11 @@ public class AccountController extends AbstractController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserDTO> getCurrentUser(HttpServletRequest req) {
+    public ResponseEntity<UserWithPermissionsDTO> getCurrentUser(HttpServletRequest req) {
         User u = AuthTokenUtil.getUser(req);
         if (u == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(new UserDTO(u));
+        return ResponseEntity.ok(new UserWithPermissionsDTO(u));
     }
 }
